36945ac4356fe83a9ba070d045ed9fa2a3b69a15
ilvxna <ilvxna@hotmail.com>
2018-05-10 18:50:12 +0800

    使用MVC重构

--- Assets/_Scripts/PlayerInventory.cs
+++ Assets/_Scripts/PlayerInventory.cs
@@ -1,30 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-public class Player : MonoBehaviour {
 
-	public Text starText;
+public class PlayerInventory : MonoBehaviour {
+
+
+	private PlayerInventoryDisplay playerInventoryDisplay;
 
 	private bool carringStar = false;
+
 	// Use this for initialization
 	void Start () {
-		
+		playerInventoryDisplay = this.gameObject.GetComponent<PlayerInventoryDisplay>();
+
+		playerInventoryDisplay.onChangeCarringStar(carringStar);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
 
+	}
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.CompareTag("Star"))
 		{
 			carringStar = true;
-			
+
+			playerInventoryDisplay.onChangeCarringStar(carringStar);
+
 			Destroy(collision.gameObject);
-		}		
-	} 
+		}
+	}
 
 }
---
+++ Assets/_Scripts/PlayerInventoryDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerInventoryDisplay : MonoBehaviour {
+
+
+	public Text starText;
+	public void onChangeCarringStar(bool carringStar)
+	{
+		string starMsg = "no star:-(";
+
+		if (carringStar)
+		{
+			starMsg = "Carring Star :-)";
+		}
+		starText.text = starMsg;
+	}
+
+
+}
